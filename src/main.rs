0f7ba10039f0f6e6823064842c736cc8e70b99c4
trentgillin <trentgillin@gmail.com>
2020-09-09 21:35:51 -0400

    Working on adding in train

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ extern crate serde_derive;
 extern crate structopt;
 use reqwest::Error;
 use serde::Deserialize;
-use structopt::StructOpt;
+use std::env;
 use std::fmt;
+use std::io;
+use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "Rusty Marta", about = "A small CLI to pull MARTA bus data")]
+#[structopt(name = "Rusty Marta", about = "A small CLI to pull MARTA data")]
 struct Opt {
     // bus number
-    #[structopt(short)]
+    #[structopt(short, default_value = "No Bus Selected")]
     bus: String,
+
+    // train route
+    #[structopt(short, default_value = "Not Selected")]
+    train: String,
 }
 
 #[derive(Deserialize)]
@@ -23,32 +29,60 @@ struct Bus {
     vehicle: String,
 }
 
-impl fmt::Display for Bus{
+impl fmt::Display for Bus {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Station: {} | Time Away: {} | Bus ID: {}", self.timepoint, self.adherence, self.vehicle)
+        write!(
+            f,
+            "Station: {} | Time Away: {} | Bus ID: {}",
+            self.timepoint, self.adherence, self.vehicle
+        )
     }
 }
 
 fn main() -> Result<(), Error> {
     let opt = Opt::from_args();
 
-    let request_url = format!(
-        "http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetBusByRoute/{}",
-        opt.bus
-    );
+    if opt.bus != "No Bus Selected" {
+        let request_url = format!(
+            "http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetBusByRoute/{}",
+            opt.bus
+        );
 
-    let response = reqwest::blocking::get(&request_url)?;
-    let bus: Vec<Bus> = response.json()?;
+        let response = reqwest::blocking::get(&request_url)?;
+        let bus: Vec<Bus> = response.json()?;
 
-    // handle buses with no times currently
-    if bus.len() == 0 {
-        println!("No times for bus: {}", opt.bus)
+        // handle buses with no times currently
+        if bus.len() == 0 {
+            println!("No times for bus: {}", opt.bus)
+        } else {
+            println!("There are {} bus(es) running", bus.len());
+            println!("=========================");
+            for i in bus {
+                println!("{}", i);
+            }
+        };
     } else {
-        println!("There are {} buses running", bus.len());
-        println!("=========================");
-        for i in bus {
-            println!("{}", i); 
-        }
+        // Does the user have a Marta API Token stored in the environment variables
+        let test_token = env::var("MARTA_TOKEN").is_err();
+
+        let test_token = if test_token {
+            println!("You Need a MARTA API Token, you can get one at https://www.itsmarta.com/developer-reg-rtt.aspx");
+            println!("Please enter your API Token or hit Ctrl+C to end the program:");
+
+            // Get Users token
+            let mut token = String::new();
+
+            io::stdin()
+                .read_line(&mut token)
+                .expect("Failed to read line");
+
+            // set to environment variable
+            let marta_token = "MARTA_TOKEN";
+            env::set_var(marta_token, &token);
+        };
+
+        let marta_token = env::var("MARTA_TOKEN");
+        println!("{:?}", marta_token);
     };
     Ok(())
 }
